6f975ab51b2f8f37c92a382a300106aef97f1db5
zys8119 <770959294@qq.com>
2021-11-22 14:52:28 +0800

    [add] 项目创建

---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+import {Plugin, App} from "vue"
+declare const remPlug :Plugin & remPlugInstall;
+type remPlugInstall  = {
+    install(app:App,  ...options: remPlugOptions[]):void;
+}
+type remPlugOptions = {
+    // 设计稿基准
+    base:number | 0;
+    // 是否为弹框插件模式，原因是弹框插件需要每次实例化
+    plug:boolean;
+}
+export = remPlug;
