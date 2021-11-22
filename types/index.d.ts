c1c03a19f79ccf7699f0543f58d2098419a0a674
zys8119 <770959294@qq.com>
2021-11-22 14:58:23 +0800

    [add] 项目创建

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,9 +4,13 @@ type remPlugInstall  = {
     install(app:App,  ...options: remPlugOptions[]):void;
 }
 type remPlugOptions = {
-    // 设计稿基准
+    /**
+     * 设计稿基准
+     */
     base:number | 0;
-    // 是否为弹框插件模式，原因是弹框插件需要每次实例化
+    /**
+     * 是否为弹框插件模式，原因是弹框插件需要每次实例化
+     */
     plug:boolean;
 }
 export = remPlug;
